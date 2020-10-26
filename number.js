92973cdf553741216cf2b0480306361a418d74d2
Thierry GEGOUX <61021561+T-G3X@users.noreply.github.com>
2020-10-26 11:25:50 +0100

    Add files via upload
    Number of trials added
    GUI improvement

--- number.js
+++ number.js
@@ -2,6 +2,10 @@ let numberToFind = Math.floor(Math.random() * 101)
 let submit = document.getElementById("button")
 let userValue = document.getElementById("userValue")
 let showMessage = document.getElementById("result")
+let node = document.getElementById("submit")
+let parentNode = document.getElementById("form")
+let button = document.getElementById("button1")
+let i = 0
 
 
 console.log(numberToFind)
@@ -10,10 +14,12 @@ console.log(numberToFind)
 userValue.addEventListener("keydown", function(event){
     if (event.keyCode === 13){
         event.preventDefault()
-        //console.log("fuck you !")
         getInputValue()
+        i = i + 1
     }
+    console.log (i);
 })
+
 
 //compare user's number with number to find
 function getInputValue(){
@@ -24,7 +30,9 @@ function getInputValue(){
     else if (numberValue <= 100 && numberValue > numberToFind) {
         showMessage.innerHTML = "Trop grand"
     } else if (numberValue === numberToFind){
-        showMessage.innerHTML = "GAGNE !"
+        parentNode.removeChild(node)
+        button.removeChild(button2)
+        showMessage.innerHTML = "Bravo vous avez gagné !" + "<br>" + "<br>" + " Nombre de tentatives " + (i+1)
     }
     else {
         showMessage.innerHTML = "Vous n'avez pas saisi un chiffre compris entre 0 et 100 !"
